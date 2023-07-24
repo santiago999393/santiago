43b5c80ebaab8732c0afb87e6569c7e0475437e7
santiago999393 <140454153+santiago999393@users.noreply.github.com>
2023-07-24 16:20:05 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.pause(500)
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+})
